eebfb548fbc4def8550761781ef8126dfa32e579
adrioro <artistsvillage@me.com>
2019-01-22 09:57:38 -0500

    Fourth commit

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var box = document.getElementById('box');
 box.style.width = '100px';
 box.style.height = '100px';
 box.style.backgroundColor = "grey";
-box.style.border = '1px solid black';
 box.innerText = "Hello, I am a box";
-box.style.color = "white";
+
+//Simplification
+
+boxStyle = box.style;
+
+boxStyle.border = '1px solid black';
+boxStyle.color = "white";
+boxStyle.padding = "10px";
