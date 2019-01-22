881a8bfa8fa3cd2361d6187c24b28bee5302de25
adrioro <artistsvillage@me.com>
2019-01-22 09:52:28 -0500

    Third commit

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ box.style.width = '100px';
 box.style.height = '100px';
 box.style.backgroundColor = "grey";
 box.style.border = '1px solid black';
+box.innerText = "Hello, I am a box";
+box.style.color = "white";
